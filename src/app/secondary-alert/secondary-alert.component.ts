9b27d2f2a69a50755cfdc4bc6d92a0d58cfff920
olyaoezel <olga.oezel@btb.gmbh>
2022-02-21 15:01:43 +0100

    config

---
+++ src/app/secondary-alert/secondary-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-alert',
+  templateUrl: './secondary-alert.component.html',
+  styleUrls: ['./secondary-alert.component.css']
+})
+export class SecondaryAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
