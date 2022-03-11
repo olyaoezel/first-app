dfa1172ec27cba9497dd8fd6f7689084a1124619
olyaoezel <olga.oezel@btb.gmbh>
2022-03-11 08:40:53 +0100

    fixed links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, )
   ],
   providers: [],
   bootstrap: [AppComponent]
