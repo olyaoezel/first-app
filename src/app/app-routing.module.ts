67112c59608b0f03824bb07b1aada8dc167e0d8c
olyaoezel <olga.oezel@btb.gmbh>
2022-03-14 11:01:25 +0100

    outsourced the route configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home/home.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { ServersComponent } from "./servers/servers.component";
+import { NewSubscriptionsComponent } from "./users/new-subscriptions/new-subscriptions/new-subscriptions.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'messages', component: MessagesComponent },
+    { path: 'servers', component: ServersComponent },
+    { path: 'users', component: UsersComponent },
+    { path: 'users/new-subscriptions', component: NewSubscriptionsComponent },
+    { path: 'users/:id/:name', component: UserComponent },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ], exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,22 +16,13 @@ import { BasicHightlightDirective } from './directives/basic-hightlight.directiv
 import { UnlessDirective } from './directives/unless.directive';
 import { ActiveUsersComponent } from './users/active-users/active-users.component';
 import { InactiveUsersComponent } from './users/inactive-users/inactive-users.component';
-import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home/home.component';
 import { NewSubscriptionsComponent } from './users/new-subscriptions/new-subscriptions/new-subscriptions.component';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
 import { UserComponent } from './users/user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'messages', component: MessagesComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/new-subscriptions', component: NewSubscriptionsComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  
-]
 
 @NgModule({
   declarations: [
@@ -54,12 +45,12 @@ const appRoutes: Routes = [
     NewSubscriptionsComponent,
     ClickOutsideDirective,
     UserComponent,
-  
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
